1acab9a349efc5b94044f9b09da663ac4f1766cf
liliniu <liliniu@outlook.com>
2020-01-23 17:22:23 +0100

    Fixed bug

--- text.py
+++ text.py
@@ -1,1 +1,24 @@
 ##test
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LinearRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from scipy.stats import zscore
+import itertools
+
+from sklearn.metrics import accuracy_score
+from sklearn.metrics import roc_auc_score, mean_squared_error, roc_curve, auc, accuracy_score, f1_score, recall_score
+from sklearn.metrics import precision_recall_curve
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import average_precision_score
+from inspect import signature
+
+from sklearn.feature_selection import mutual_info_classif
+from sklearn.feature_selection import mutual_info_regression
+from sklearn.feature_selection import SelectFromModel
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from xgboost import XGBClassifier
+from sklearn.model_selection import RepeatedStratifiedKFold
+from sklearn.metrics import matthews_corrcoef
---
+++ venv_ald/lib/python3.6/weakref.py
@@ -0,0 +1,1 @@
+/Users/niulili/anaconda3/lib/python3.6/weakref.py
