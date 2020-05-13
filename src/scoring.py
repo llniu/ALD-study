71a648daa5666395e99ac9e478839c4dc6916bf9
enryh <henry.webel@sund.ku.dk>
2020-05-13 16:38:47 +0200

    confusion matrix as pandas.DataFrame

---
+++ src/scoring.py
@@ -0,0 +1,23 @@
+
+import pandas as pd
+import sklearn
+import sklearn.metrics as sklm
+
+class ConfusionMatrix():
+    """Wrapper for `sklearn.metrics.confusion_matrix`"""
+    def __init__(self, y_true, y_pred):
+        self.cm_ = sklm.confusion_matrix(y_true, y_pred)
+    
+    @property
+    def as_dataframe(self):
+        if not hasattr(self, 'df'):
+            self.df = pd.DataFrame(self.cm_)
+            self.df.index.name = 'true'
+            self.df.columns.name = 'pred'
+        return self.df
+    
+    def __str__(self):
+        return str(self.cm_)
+    
+    def __repr__(self):
+        return repr(self.cm_)
