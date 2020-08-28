98a1df6e7b6f7a47be1c08d1d517e3ca4e256673
enryh <henry.webel@cpr.ku.dk>
2020-08-28 12:24:18 +0200

    correct omission of keys from second on series

--- src/pandas.py
+++ src/pandas.py
@@ -1,6 +1,6 @@
 import pandas as pd
 
-def combine_value_counts(X:pd.DataFrame):
+def combine_value_counts(X:pd.DataFrame, dropna=True):
     """Pass a selection of columns to combine it's value counts.
     
     This performs no checks. Make sure the scale of the variables
@@ -8,7 +8,7 @@ def combine_value_counts(X:pd.DataFrame):
     """
     _df = pd.DataFrame()
     for col in X.columns:
-        _df[col] = X[col].value_counts()
+        __df = _df.join(X[col].value_counts(dropna=dropna), how='outer')
     freq_targets = _df.sort_index()
     return freq_targets
 
